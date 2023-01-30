0ec705782f5f21e80256bdc739588c73e93a0279
zachary schwab <zach7292@gmail.com>
2023-01-30 02:59:51 -0500

    first and hopefully last commit

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Chris on 1/29/23.
+//
+
+import Foundation
+
+class ChecklistItem: NSObject {
+    var text = ""
+    var checked = false
+}
